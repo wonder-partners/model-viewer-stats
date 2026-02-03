426a2526183477e4398673bae16f782ee544957d
Sov3rain <10755450+Sov3rain@users.noreply.github.com>
2026-02-03 15:19:15 +0100

    feat: add global augmentation for JSX.IntrinsicElements

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,13 @@
 import { ModelStats } from "./ModelStats.js";
 export { ModelStats };
+
+declare global {
+	namespace JSX {
+		interface IntrinsicElements {
+			"model-stats": React.DetailedHTMLProps<
+				React.HTMLAttributes<HTMLElement>,
+				HTMLElement
+			> & { visible?: boolean };
+		}
+	}
+}
